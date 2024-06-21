150736b6732830a18cb0094cf3148ab70e2e244a
Somanshu Bhardwaj <89822689+somanshubhardwaj@users.noreply.github.com>
2024-06-21 17:35:11 +0530

    chore: Update npm dependencies
    The package.json and package-lock.json files have been modified to update the dependencies. The axios and express packages have been updated to their latest versions, and the cors package has been added as a new dependency. This update is necessary to ensure compatibility and take advantage of the latest features and bug fixes provided by these packages.

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
 const express = require("express");
 const axios = require("axios");
-
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 const port = 4000;
 const token = "5532a84479c2fcd0e6f9cfd297028a8b";
-// Middleware to parse JSON requests
+
 app.use(express.json());
 
-// Route to serve the home page
 app.get("/", (req, res) => {
-  res.send("<h1>Welcome to Express with Axios Example</h1>");
+
+  res.sendFile(__dirname + '/index.html');
 });
 
-// Route to fetch data from an external API
 app.get("/api/data", async (req, res) => {
   try {
-    // Replace the URL with an actual API endpoint
     const apiUrl = "https://jsonplaceholder.typicode.com/posts";
 
-    // Making a GET request using Axios
     const response = await axios.get(apiUrl);
 
-    // Sending the fetched data back to the client
     res.json(response.data);
   } catch (error) {
     console.error("Error fetching data from API:", error);
     res.status(500).send("An error occurred while fetching data.");
   }
 });
-app.get("/api/movies/nowplaying", async (req, res) => {
+
+// ... rest of the code
+
+/**
+ * Route for getting similar movies
+ * @name GET /api/movie/:id/similar
+ * @function
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
+app.get("/api/movie/:id/similar", async (req, res) => {
   try {
     const response = await fetch(
-      `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&region=in&api_key=${token}`
+      `https://api.themoviedb.org/3/movie/${req.params.id}/similar?language=en-US&page=1&api_key=${token}`
     );
     const data = await response.json();
     res.json(data);
@@ -39,10 +47,18 @@ app.get("/api/movies/nowplaying", async (req, res) => {
     res.status(500).json({ message: e.message });
   }
 });
-app.get("/api/movies/popular", async (req, res) => {
+
+/**
+ * Route for getting similar TV shows
+ * @name GET /api/tv/:id/similar
+ * @function
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
+app.get("/api/tv/:id/similar", async (req, res) => {
   try {
     const response = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&region=in&api_key=${token}`
+      `https://api.themoviedb.org/3/tv/${req.params.id}/similar?language=en-US&page=1&api_key=${token}`
     );
     const data = await response.json();
     res.json(data);
@@ -50,94 +66,262 @@ app.get("/api/movies/popular", async (req, res) => {
     res.status(500).json({ message: e.message });
   }
 });
-app.get("/api/movies/toprated", async (req, res) => {
+
+/**
+ * Route for getting combined credits of a person
+ * @name GET /api/person/:id/combined_credits
+ * @function
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
+app.get("/api/person/:id/combined_credits", async (req, res) => {
   try {
     const response = await fetch(
-      `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&region=in&api_key=${token}`
+      `https://api.themoviedb.org/3/person/${req.params.id}/combined_credits?language=en-US&api_key=${token}`
     );
     const data = await response.json();
     res.json(data);
   } catch (e) {
     res.status(500).json({ message: e.message });
   }
-});
-app.get("/api/movies/upcoming", async (req, res) => {
+}
+);
+
+/**
+ * Route for getting movie reviews
+ * @name GET /api/movie/:id/reviews
+ * @function
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
+app.get("/api/movie/:id/reviews", async (req, res) => {
   try {
     const response = await fetch(
-      `https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1&region=in&api_key=${token}`
+      `https://api.themoviedb.org/3/movie/${req.params.id}/reviews?language=en-US&page=1&api_key=${token}`
     );
     const data = await response.json();
     res.json(data);
   } catch (e) {
     res.status(500).json({ message: e.message });
   }
-});
-app.get("/api/trending/movies", async (req, res) => {
+}
+);
+
+/**
+ * Route for getting TV show reviews
+ * @name GET /api/tv/:id/reviews
+ * @function
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
+app.get("/api/tv/:id/reviews", async (req, res) => {
   try {
     const response = await fetch(
-      `https://api.themoviedb.org/3/trending/movie/day?api_key=${token}`
+      `https://api.themoviedb.org/3/tv/${req.params.id}/reviews?language=en-US&page=1&api_key=${token}`
     );
     const data = await response.json();
     res.json(data);
   } catch (e) {
     res.status(500).json({ message: e.message });
   }
-});
-app.get("/api/trending/tv", async (req, res) => {
+}
+);
+
+/**
+ * Route for getting person images
+ * @name GET /api/person/:id/images
+ * @function
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
+app.get("/api/person/:id/images", async (req, res) => {
   try {
     const response = await fetch(
-      `https://api.themoviedb.org/3/trending/tv/day?api_key=${token}`
+      `https://api.themoviedb.org/3/person/${req.params.id}/images?language=en-US&api_key=${token}`
     );
     const data = await response.json();
     res.json(data);
   } catch (e) {
     res.status(500).json({ message: e.message });
   }
-});
-app.get("/api/trending/people", async (req, res) => {
+}
+);
+
+/**
+ * Route for getting movie images
+ * @name GET /api/movie/:id/images
+ * @function
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
+app.get("/api/movie/:id/images", async (req, res) => {
   try {
     const response = await fetch(
-      `https://api.themoviedb.org/3/trending/tv/day?api_key=${token}`
+      `https://api.themoviedb.org/3/movie/${req.params.id}/images?language=en-US&api_key=${token}`
     );
     const data = await response.json();
     res.json(data);
   } catch (e) {
     res.status(500).json({ message: e.message });
   }
-});
-app.get("/api/tv/airingtoday", async (req, res) => {
+}
+);
+
+/**
+ * Route for getting TV show images
+ * @name GET /api/tv/:id/images
+ * @function
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
+app.get("/api/tv/:id/images", async (req, res) => {
   try {
     const response = await fetch(
-      `https://api.themoviedb.org/3/tv/airing_today?language=en-US&page=1&region=in&api_key=${token}`
+      `https://api.themoviedb.org/3/tv/${req.params.id}/images?language=en-US&api_key=${token}`
     );
     const data = await response.json();
     res.json(data);
   } catch (e) {
     res.status(500).json({ message: e.message });
   }
-});
-app.get("/api/tv/popular", async (req, res) => {
+}
+);
+
+/**
+ * Route for getting movie videos
+ * @name GET /api/movie/:id/videos
+ * @function
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
+app.get("/api/movie/:id/videos", async (req, res) => {
   try {
     const response = await fetch(
-      `https://api.themoviedb.org/3/tv/popular?language=en-US&page=1&region=in&api_key=${token}`
+      `https://api.themoviedb.org/3/movie/${req.params.id}/videos?language=en-US&api_key=${token}`
     );
     const data = await response.json();
     res.json(data);
   } catch (e) {
     res.status(500).json({ message: e.message });
   }
-});
-app.get("/api/tv/toprated", async (req, res) => {
+}
+);
+
+/**
+ * Route for getting TV show videos
+ * @name GET /api/tv/:id/videos
+ * @function
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
+app.get("/api/tv/:id/videos", async (req, res) => {
   try {
     const response = await fetch(
-      `https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1&region=in&api_key=${token}`
+      `https://api.themoviedb.org/3/tv/${req.params.id}/videos?language=en-US&api_key=${token}`
     );
     const data = await response.json();
     res.json(data);
   } catch (e) {
     res.status(500).json({ message: e.message });
   }
-});
+}
+);
+app.get("/api/person/:id/tagged_images", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/person/${req.params.id}/tagged_images?language=en-US&api_key=${token}`
+    );
+    const data = await response.json();
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+}
+);
+app.get("/api/movie/:id/credits", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${req.params.id}/credits?language=en-US&api_key=${token}`
+    );
+    const data = await response.json();
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+}
+);
+app.get("/api/tv/:id/credits", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/tv/${req.params.id}/credits?language=en-US&api_key=${token}`
+    );
+    const data = await response.json();
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+}
+);
+app.get("/api/person/:id/external_ids", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/person/${req.params.id}/external_ids?language=en-US&api_key=${token}`
+    );
+    const data = await response.json();
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+}
+);
+app.get("/api/movie/:id/external_ids", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${req.params.id}/external_ids?language=en-US&api_key=${token}`
+    );
+    const data = await response.json();
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+}
+);
+app.get("/api/tv/:id/external_ids", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/tv/${req.params.id}/external_ids?language=en-US&api_key=${token}`
+    );
+    const data = await response.json();
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+}
+);
+app.get("/api/movie/:id/recommendations", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${req.params.id}/recommendations?language=en-US&page=1&api_key=${token}`
+    );
+    const data = await response.json();
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+}
+);
+app.get("/api/tv/:id/recommendations", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/tv/${req.params.id}/recommendations?language=en-US&page=1&api_key=${token}`
+    );
+    const data = await response.json();
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+}
+);
 
 // Start the Express server
 app.listen(port, () => {
